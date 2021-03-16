742b872cebb5309aedf3e70d02f027b72218132a
njal3001 <73302347+njal3001@users.noreply.github.com>
2021-03-16 16:29:14 +0100

    Initial files

---
+++ t-sne.py
@@ -0,0 +1,15 @@
+from utility import k_nearest_neighbors
+
+def t_sne(data_set, k):
+    knn = k_nearest_neighbors(data_set, k)
+
+
+
+
+
+
+
+
+
+
+
---
+++ utility.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def distance_matrix(matrix):
+    """Computes the distance between each point in the input matrix.
+        Returns a result matrix r where r[i, j] is the distance between point i and point j."""
+    #for point in matrix:
+    #   pass
+
+
+
+def k_nearest_neighbors(d_matrix, k):
+    """Computes the k nearest neighbors of each point given a distance matrix.
+       Returns a result matrix r where r[i, j] is 0 if point j is not one of point i's k nearest neighbors."""
+    #r = []
+    #for row in d_matrix:
+    #    k_largest = np.partition(row, -k)[-k]
+
