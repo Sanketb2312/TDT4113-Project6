0a3b530dbdca2a5414540742a00dc4cac90b4cd2
sanketb <sanketb@stud.ntnu.no>
2021-04-03 16:58:01 +0200

    pca finished with correct plot in 2D format. TODO: the sprial is inverted

--- isomap.py
+++ isomap.py
@@ -1,1 +1,6 @@
 #skal væra method i følge oppgavaeteksten
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.sparse.linalg import eigs
+
+
--- pca.py
+++ pca.py
@@ -1,35 +1,56 @@
 #skal væra class i følge oppgavaeteksten
 import numpy as np
 import csv
+import matplotlib.pyplot as plt
 from scipy.sparse.linalg import eigs, eigsh
 class PCA:
     def __init__(self):
-        self.numbers = []
+        self.data =''
 
-    def read_data(self):
-        data = np.genfromtxt('data_files/digits.csv', delimiter=',')
-        print(data)
-        return data
+    def read_data(self, path):
+        self.data = np.genfromtxt(path, delimiter=',')
+
 
     def fit(self):
-        my = self.read_data().mean()
-        centered_matrix = self.read_data()-my
-        cov_matrix = np.cov(centered_matrix)
-        print(cov_matrix)
-        D = len(self.read_data())
-        if D - 1 > 1:
-            [eigenvalues, eigenvectors]= eigs(cov_matrix, k=1)
-        elif D-1 == 1:
-            [eigenvalues, eigenvectors] = eigsh(cov_matrix, k=1)
-        return [eigenvalues, eigenvectors]
+        centered_matrix = self.data-self.data.mean(axis=0)
+        cov_matrix = np.cov(centered_matrix.transpose())
+        D = self.data.shape[1]
+        print(D)
+        if D - 1 > 2:
+            [eigenvalues, eigenvectors] = eigs(cov_matrix, k=2)
+        else:
+            [eigenvalues, eigenvectors] = eigsh(cov_matrix, k=2)
+            ind_sorted = np.argsort(eigenvalues)
+            eigenvectors = eigenvectors[:, ind_sorted[-2:]]
+            eigenvectors = np.real(eigenvectors)
+        return eigenvectors
 
 
-        def transform(self):
-            pass
+    def transform(self):
+        centered_matrix = self.data-self.data.mean(axis=0)
+        f = self.fit()
+        trans = f.transpose()
+        return (trans@centered_matrix.transpose()).transpose()
+
+    def plot(self, color):
+        y_plot = self.transform()
+        print(y_plot)
+        if color == 2000:
+            c_c = np.arange(color)
+            plt.scatter(y_plot[:, 0], y_plot[:, 1], s=10, c=c_c, marker=".", cmap='jet')
+            plt.show()
+        elif color == 5620:
+            color_plot = np.genfromtxt('data_files/digits_label.csv', delimiter=',')
+            plt.scatter(y_plot[:, 0], y_plot[:, 1], s=10, c=color_plot, marker=".", cmap='jet')
+            plt.show()
 
 
 
 
-p = PCA()
-p.read_data()
-p.fit()
+
+if __name__ == '__main__':
+    pca = PCA()
+    pca.read_data('data_files/swiss_data.csv')
+    pca.plot(2000)
+    pca.read_data('data_files/digits.csv')
+    pca.plot(5620)
