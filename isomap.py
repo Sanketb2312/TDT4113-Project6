44e3953ae9bb465eaf8b90bed99de01638642dc5
Hary Pirajan Mahalingam <hary2309@gmail.com>
2021-04-05 20:10:51 +0200

    Isomap er implementert

--- isomap.py
+++ isomap.py
@@ -2,5 +2,83 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.sparse.linalg import eigs
+from sklearn.utils.graph_shortest_path import graph_shortest_path
+
+from utility import k_nearest_neighbors, pairwise_euclidean_distance
 
 
+def compute_geodesics(d_matrix, k):
+    dist_matrix = pairwise_euclidean_distance(d_matrix)
+    d_kNN = k_nearest_neighbors(dist_matrix, k)
+    return graph_shortest_path(d_kNN)
+
+
+def multidimensional_scaling(d_geodesics):
+    print("MSD Starting")
+    # Task a)
+    d_2 = np.square(d_geodesics)
+
+    # Task b)
+    rows = len(d_2)
+    tmp = [None] * rows
+    '''j_matrix = []
+    for i in range(rows):
+        j_matrix.append(tmp.copy())
+    for i in range(rows):
+        for j in range(rows):
+            if i == j:
+                j_matrix[i][j] = 1 - 1 / (rows)
+            else:
+                j_matrix[i][j] = 0 - 1 / (rows)'''
+    j_matrix = np.identity(rows, dtype=int)-1/rows
+
+    j_x_d2 = np.matmul(j_matrix, d_2)
+    j_x_d2_x_j = np.matmul(j_x_d2, j_matrix)
+    b = j_x_d2_x_j * (-1 / 2)
+
+
+    # Task c)
+    m = 2
+    eigvals, eigvecs = np.linalg.eig(b) # FØLER eigh er feil, skrev eig istden
+
+    sortedIndexes = eigvals.argsort()[::-1]
+    m_largest_eigvals = eigvals[sortedIndexes][0:m]
+    sorted_eigvecs = (eigvecs.transpose()[sortedIndexes]).transpose()
+    m_largest_eigvecs = sorted_eigvecs[:, 0:m]
+
+    # Task d)
+    E_m = m_largest_eigvecs
+    tmp = [0]*m
+    A_1_2 = []
+    for i in range(m):
+        A_1_2.append(tmp.copy())
+        A_1_2[i][i] = np.sqrt(m_largest_eigvals[i]) # SKal egt være A_1_2[i][i] = np.sqrt(m_largest_eigvals[i])
+
+    Y = np.matmul(E_m, A_1_2)
+    print(Y)
+    return Y
+
+if __name__ == '__main__':
+
+    #d_matrix = np.genfromtxt('data_files/swiss_data_fake.csv', delimiter=',')
+    choose = 1
+
+    if choose == 0:
+        d_matrix = np.genfromtxt('data_files/swiss_data.csv', delimiter=',')
+        k = 30
+        d_geodesics = compute_geodesics(d_matrix, k)
+        mds = multidimensional_scaling(d_geodesics).transpose()
+        plt.scatter(mds[0], mds[1], c=np.arange(2000), cmap="jet")
+
+    if choose == 1:
+        d_matrix = np.genfromtxt('data_files/digits.csv', delimiter=',')
+        k = 50
+        d_geodesics = compute_geodesics(d_matrix, k)
+        mds = multidimensional_scaling(d_geodesics).transpose()
+        plt.scatter(mds[0], mds[1], c=np.genfromtxt('data_files/digits_label.csv'), cmap="jet", s=10, marker=".")
+
+
+
+
+
+    plt.show()
--- utility.py
+++ utility.py
@@ -1,5 +1,4 @@
 import numpy as np
-from sklearn.utils.graph_shortest_path import graph_shortest_path
 
 
 def square_diff(point1, point2):
@@ -14,99 +13,45 @@ def square_diff(point1, point2):
     return sum_of_p1s_squared + sum_of_p2s_squared - 2 * sum_of_p1p2s
 
 
-def pairwise_euclidean_distance(high_dimensional_data_points):
+def pairwise_euclidean_distance(data_points):
     """Computes the distance between each point in the input matrix.
         Returns a result matrix r where r[i, j] is the distance between point i and point j."""
-    rows = len(high_dimensional_data_points)
-    square_diff_matrix = []
-    for amount in range(rows):
-        pass
-
-    temp = []
-    for i in range(rows):
-        temp.append(0)
-    for i in range(rows):
-        square_diff_matrix.append(temp.copy())
-
-    already_calculated_to = 0
-    for i in range(rows):
-        for j in range(already_calculated_to, rows):
-            if i != j:
-                square_diff_matrix[i][j] = square_diff(high_dimensional_data_points[i],
-                                                       high_dimensional_data_points[j])
-                square_diff_matrix[j][i] = square_diff_matrix[i][j]
-        already_calculated_to += 1
-    return square_diff_matrix
+    d_1 = np.sum(data_points ** 2, axis=1, keepdims=True)
+    d_2 = np.sum(data_points ** 2, axis=1)
+    d_3 = np.dot(data_points, data_points.T)
+    return np.sqrt(np.abs(d_1 + d_2 - 2 * d_3))
 
 
 def k_nearest_neighbors(d_matrix, k):
     """Computes the k nearest neighbors of each point given a distance matrix.
        Returns a result matrix r where r[i, j] is 0 if point j is not one of point i's k nearest neighbors."""
+    print("jsakhdkjsah")
+
+    k_matrix = d_matrix.copy()
     rows = len(d_matrix)
-
-    already_calculated_to = 0
     for i in range(rows):
-        # k+1 to avoid counting already existing
-        for j in range(rows - k - 1):
-            max_value = max(d_matrix[i])
-            max_index = d_matrix[i].index(max_value)
-            d_matrix[i][max_index] = 0
-        already_calculated_to += 1
-    return d_matrix
-
-
-def compute_geodesics(d_matrix, k):
-    d_kNN = k_nearest_neighbors(pairwise_euclidean_distance(d_matrix), k)
-    return graph_shortest_path(d_kNN)
-
-
-def multidimensional_scaling(d_geodesics):
-    # Task a)
-    d_2 = np.multiply(d_geodesics, d_geodesics)
-
-    # Task b)
-    rows = len(d_2)
-    tmp = [None] * rows
-    j_matrix = []
-    for i in range(rows):
-        j_matrix.append(tmp.copy())
-    for i in range(rows):
-        for j in range(rows):
-            if i == j:
-                j_matrix[i][j] = 1 - 1 / (rows ** 2)
-            else:
-                j_matrix[i][j] = 0 - 1 / (rows ** 2)
-    d2_x_j = np.matmul(d_2, j_matrix)
-    b = d2_x_j * (-1 / 2)
-
-    # Task c)
-    m = 2
-    eigvals, eigvecs = np.linalg.eig(b)
-    m_largest_eigvecs = [None, None]
-    m_largest_eigvals = [None, None]
-    for i in range(len(eigvals)):
-        for j in range(len(m_largest_eigvals)):
-            if m_largest_eigvals[j] is None or eigvals[i] > m_largest_eigvals[j]:
-                m_largest_eigvals[j] = eigvals[i]
-                m_largest_eigvecs[j] = eigvecs[:, i]
-    print(m_largest_eigvecs)
-    return m_largest_eigvecs
+        #print(i)
+        sortedIndexes = np.argsort(d_matrix[i])
+        for index in sortedIndexes[k + 1:]:
+            k_matrix[i][index] = 0
+            #print(index)
+    print("done")
+    return k_matrix
 
 
 if __name__ == '__main__':
     x = [1, 2, 3]
     y = [2, 3, 4]
-    #print(square_diff(x, y))
+    # print(square_diff(x, y))
 
     x1 = [4]
     y2 = [2]
-    #print(square_diff(x1, y2))
+    # print(square_diff(x1, y2))
 
     x2 = [[1, 2, 3, 2, 4], [2, 4, 3, 3, 2], [2, 4, 4, 3, 1], [2, 4, 3, 3, 1], [2, 2, 4, 3, 1],
           [0, 0, 1, 0, 0]]
 
-    #print(pairwise_euclidean_distance(x2))
+    # print(pairwise_euclidean_distance(x2))
 
-    #print(k_nearest_neighbors(pairwise_euclidean_distance(x2), 3))
+    # print(k_nearest_neighbors(pairwise_euclidean_distance(x2), 3))
 
-    multidimensional_scaling(compute_geodesics(x2, 3))
